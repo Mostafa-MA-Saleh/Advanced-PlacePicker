d283dcdfa12e2a99bfde04e75eaf2fabafd2948c
Mostafa Saleh <mostafa.saleh@bugzy.net>
2017-08-14 15:58:12 +0200

    - Removed caching.

--- advancedplacepicker/src/main/java/saleh/ma/mostafa/gmail/com/advancedplacepicker/utilities/AddressResolver.java
+++ advancedplacepicker/src/main/java/saleh/ma/mostafa/gmail/com/advancedplacepicker/utilities/AddressResolver.java
@@ -5,9 +5,6 @@ import android.support.annotation.Nullable;
 
 import com.google.android.gms.maps.model.LatLng;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import saleh.ma.mostafa.gmail.com.advancedplacepicker.network.NetworkManager;
 
 /**
@@ -17,10 +14,8 @@ import saleh.ma.mostafa.gmail.com.advancedplacepicker.network.NetworkManager;
 public class AddressResolver {
 
     private static AddressResolver mInstance = new AddressResolver();
-    private Map<LatLng, String> addressCache;
 
     private AddressResolver() {
-        addressCache = new HashMap<>();
     }
 
     public static AddressResolver getInstance() {
@@ -28,22 +23,16 @@ public class AddressResolver {
     }
 
     public void getAddress(Context context, final LatLng coordinates, final OnFinishedListener<String> onFinishedListener) {
-        String address = addressCache.get(coordinates);
-        if (address != null) {
-            onFinishedListener.onSuccess(address);
-        } else {
-            NetworkManager.getInstance().getAddress(context, coordinates, new OnFinishedListener<String>() {
-                @Override
-                public void onSuccess(@Nullable String obj) {
-                    addressCache.put(coordinates, obj);
-                    onFinishedListener.onSuccess(obj);
-                }
+        NetworkManager.getInstance().getAddress(context, coordinates, new OnFinishedListener<String>() {
+            @Override
+            public void onSuccess(@Nullable String obj) {
+                onFinishedListener.onSuccess(obj);
+            }
 
-                @Override
-                public void onFailure(String errorMessage, int errorCode) {
-                    onFinishedListener.onFailure(errorMessage, errorCode);
-                }
-            });
-        }
+            @Override
+            public void onFailure(String errorMessage, int errorCode) {
+                onFinishedListener.onFailure(errorMessage, errorCode);
+            }
+        });
     }
 }
